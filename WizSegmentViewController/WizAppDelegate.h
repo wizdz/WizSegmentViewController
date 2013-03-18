7a483a129f2a9670c0321812a82c34112087aadf
dzpqzb <dz@wiz.cn>
2013-03-18 10:08:13 +0800

    Initial Commit

---
+++ WizSegmentViewController/WizAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  WizAppDelegate.h
+//  WizSegmentViewController
+//
+//  Created by dzpqzb on 13-3-18.
+//  Copyright (c) 2013年 wiz.cn. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface WizAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
